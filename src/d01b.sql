885b70d597507346217d267c117cd4b319c6bde3
Chris West (Faux) <git@goeswhere.com>
2025-12-01 10:35:29 +0000

    d01b

--- src/d01b.sql
+++ src/d01b.sql
@@ -1,22 +1,24 @@
 create table raw as
     select nr, elem as txt
     from unnest(string_to_array(
-            (select content from read_text('d01t.txt')), E'\n'))
+            (select content from read_text('d01.txt')), E'\n'))
         with ordinality a(elem, nr);
 create table num as
     select nr, (replace(replace(txt, 'R', '+'), 'L', '-')::int) as v
     from raw where txt != '';
-insert into num values (0, 50);
-create table spin as
-    select gn, mod(100+mod(
-                   (select sum(v) from num where nr <= gn), 100), 100) curr
-    from generate_series(0, (select count(*) from num) - 1) as gs(gn);
 
-create table passed as
-    select spin.*, num.v, curr+v,
-            (curr != 0 and (curr+v < 0 or curr+v > 100)) pass
-    from spin left join num on spin.gn + 1 = num.nr;
+create table arrs as select nr, v, (select array(select sign(v) from generate_series(1, abs(v)))) arr from num;
+create table clicks as select row_number() over (order by nr, click) ln, click from (select nr, unnest(arr) as click from arrs order by nr) a;
+create unique index clicks_ln on clicks (ln);
+vacuum analyze clicks;
 
-select * from passed where pass or curr = 0;
+-- 5m45s
+-- select count(*) from (
+--     select gn, mod(1000000000050+
+--                    (select sum(click) from clicks where ln <= gn), 100) curr
+--     from generate_series(1, (select count(*) from clicks)) as gs(gn)
+--               ) spin where curr=0;
 
--- 2385 to low
+-- instant:
+create table run as select ln, sum(click) over (order by ln) as curr from clicks;
+select count(*) from run where mod(1000000000050 + curr, 100) = 0;
