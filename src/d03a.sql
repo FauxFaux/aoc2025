544c9d3d81b1ce0957acfb957f8e16abefa409c4
Chris West (Faux) <git@goeswhere.com>
2025-12-03 12:05:54 +0000

    d03a

--- src/d03a.sql
+++ src/d03a.sql
@@ -1,7 +1,17 @@
 create table raw as
     select regexp_split_to_array(elem, '') bank
         from unnest(string_to_array(
-            (select content from read_text('d03t.txt')), E'\n')) a(elem);
-select array(
-    select elem from (select elem, nr from unnest(bank) with ordinality a(elem, nr) order by elem desc limit 2) t order by nr
-) from raw;
+            (select content from read_text('d03.txt')), E'\n')) a(elem);
+
+select sum(best::bigint) from (
+  select bank,
+    (select max(pair)
+        from (select
+                (bank[n] ||
+                 (select max(i)
+                    from unnest(bank[n+1:array_length(bank)]) u(i))
+                ) as pair
+               from generate_series(1, array_length(bank) - 1) gs(n))
+            pairs
+        ) best
+    from raw) by_bank;
