a7dd0349556c11b532f4620db14935eb0bb1a733
Chris West (Faux) <git@goeswhere.com>
2025-12-05 10:49:06 +0000

    d05b: duck: not implemented

--- src/d05b.sql
+++ src/d05b.sql
@@ -1,7 +1,7 @@
 create table parts as
     select n, elem
     from unnest(string_to_array(
-            (select content from read_text('d05.txt')), E'\n\n')) with ordinality a(elem, n);
+            (select content from read_text('d05s.txt')), E'\n\n')) with ordinality a(elem, n);
 
 create table ranges as
     select a[1]::bigint as start, a[2]::bigint as stop from (
@@ -9,15 +9,16 @@ create table ranges as
             from unnest(string_to_array((select elem from parts where n=1), E'\n')) a(elem)) t;
 
 select sum(stop-start+1) from ranges;
+create view overps as
+    select a.start art, a.stop aop, b.start brt, b.stop bop
+        from ranges a, ranges b
+        where a.start <= b.stop and b.start <= a.stop
+            and (a.start != b.start or a.stop != b.stop)
+            and a.start <= b.start and a.stop <= b.stop
+        order by a.start, a.stop, b.start, b.stop;
 
-create table overps as
-select a.start art, a.stop aop, b.start brt, b.stop bop, a.stop-b.start+1 width from ranges a, ranges b
-    where a.start <= b.stop and b.start <= a.stop
-        and (a.start != b.start or a.stop != b.stop)
-        and a.start <= b.start
-    order by a.start, b.start;
+select * from overps;
 
-select sum(width) from overps;
-
--- 308368198594539 too low
-select (select sum(stop-start+1) from ranges)-(select sum(width) from overps);
+with picked as (select * from overps limit 1),
+     cleaned as (delete from ranges where start = picked.start and stop = picked.stop returning *)
+select sum(stop-start+1) from cleaned;
