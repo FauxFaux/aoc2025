2af29a01ea96b2c8fbb923ad5a0cf621b42eb81a
Chris West (Faux) <git@goeswhere.com>
2025-12-10 16:00:46 +0000

    d10b: bracket error on the reduce??

--- src/d10b.sql
+++ src/d10b.sql
@@ -16,37 +16,92 @@ create table machines as
 -- select *, (select b from unnest(button) u(b) order by length(b) desc, b limit 1) long from machines o;
 -- select count(*),length(target) from machines group by 2 order by 1;
 
-create table b0 as select * from machines where nr=3;
+create table b0 as select * from machines;
 
-create table max_presses as
-    select nr,bi,
-        reduce(array_filter(target, lambda x,n: (n-1) in button[bi]),
-            lambda a,b: least(a,b)) max_presses
-    from b0,
-        generate_series(1, length(button)) gs(bi) order by nr,bi;
+-- create table max_presses as
+--     select nr,bi,
+--         reduce(array_filter(target, lambda x,n: (n-1) in button[bi]),
+--             lambda a,b: least(a,b)) max_presses
+--     from b0,
+--         generate_series(1, length(button)) gs(bi) order by nr,bi;
 
 create table by_target as
-    select nr,ti,array_agg(bi) parts,any_value(target)[ti] eq, length(any_value(button)) as btns
+    select distinct nr,array_agg(bi order by bi) parts,any_value(target)[ti] eq, length(any_value(button)) as btns
     from b0,
          generate_series(1, length(button)) gs(bi),
          generate_series(1, length(target)) gs2(ti)
-    where ((ti-1) in button[bi]) group by 1,2 order by 1,2;
+    where ((ti-1) in button[bi]) group by 1, ti order by 1,2;
 
-select * from max_presses;
+-- create unique index by_target_npe on by_target (nr, parts, eq);
+
 select * from by_target;
 
-select min(lenc) from (
-select *,reduce(guess,lambda a,b: a+b) lenc from (
-select count(distinct ti) c, guess, any_value(btns) btns from (
-select ti,[x1,x2,x3,x4,x5,x6] guess, btns,
-       reduce(parts, lambda acc, i: acc+[x1,x2,x3,x4,x5,x6][i], 0) summed, eq
-from
-    by_target,
-    generate_series(0, (select max_presses from max_presses where bi=1)) gs(x1),
-    generate_series(0, coalesce((select max_presses from max_presses where bi=2), 0)) gs(x2),
-    generate_series(0, coalesce((select max_presses from max_presses where bi=3), 0)) gs(x3),
-    generate_series(0, coalesce((select max_presses from max_presses where bi=4), 0)) gs(x4),
-    generate_series(0, coalesce((select max_presses from max_presses where bi=5), 0)) gs(x5),
-    generate_series(0, coalesce((select max_presses from max_presses where bi=6), 0)) gs(x6),
-where summed=eq) q group by guess) where c=(select count(*) from by_target)
-    );
+insert into by_target select l.nr, filter(l.parts, lambda x,n: not (x in r.parts)) parts, l.eq-r.eq eq, l.btns from by_target l, by_target r where l.nr=r.nr and l.parts @> r.parts and l.parts != r.parts order by l.nr;
+create table t as select distinct * from by_target; drop table by_target; alter table t rename to by_target;
+select count(*) from by_target;
+
+insert into by_target select l.nr, filter(l.parts, lambda x,n: not (x in r.parts)) parts, l.eq-r.eq eq, l.btns from by_target l, by_target r where l.nr=r.nr and l.parts @> r.parts and l.parts != r.parts order by l.nr;
+create table t as select distinct * from by_target; drop table by_target; alter table t rename to by_target;
+select count(*) from by_target;
+
+insert into by_target select l.nr, filter(l.parts, lambda x,n: not (x in r.parts)) parts, l.eq-r.eq eq, l.btns from by_target l, by_target r where l.nr=r.nr and l.parts @> r.parts and l.parts != r.parts order by l.nr;
+create table t as select distinct * from by_target; drop table by_target; alter table t rename to by_target;
+select count(*) from by_target;
+
+insert into by_target select l.nr, filter(l.parts, lambda x,n: not (x in r.parts)) parts, l.eq-r.eq eq, l.btns from by_target l, by_target r where l.nr=r.nr and l.parts @> r.parts and l.parts != r.parts order by l.nr;
+create table t as select distinct * from by_target; drop table by_target; alter table t rename to by_target;
+select count(*) from by_target;
+
+insert into by_target select l.nr, filter(l.parts, lambda x,n: not (x in r.parts)) parts, l.eq-r.eq eq, l.btns from by_target l, by_target r where l.nr=r.nr and l.parts @> r.parts and l.parts != r.parts order by l.nr;
+create table t as select distinct * from by_target; drop table by_target; alter table t rename to by_target;
+select count(*) from by_target;
+
+insert into by_target select l.nr, filter(l.parts, lambda x,n: not (x in r.parts)) parts, l.eq-r.eq eq, l.btns from by_target l, by_target r where l.nr=r.nr and l.parts @> r.parts and l.parts != r.parts order by l.nr;
+create table t as select distinct * from by_target; drop table by_target; alter table t rename to by_target;
+select count(*) from by_target;
+
+insert into by_target select l.nr, filter(l.parts, lambda x,n: not (x in r.parts)) parts, l.eq-r.eq eq, l.btns from by_target l, by_target r where l.nr=r.nr and l.parts @> r.parts and l.parts != r.parts order by l.nr;
+create table t as select distinct * from by_target; drop table by_target; alter table t rename to by_target;
+select count(*) from by_target;
+
+insert into by_target select l.nr, filter(l.parts, lambda x,n: not (x in r.parts)) parts, l.eq-r.eq eq, l.btns from by_target l, by_target r where l.nr=r.nr and l.parts @> r.parts and l.parts != r.parts order by l.nr;
+create table t as select distinct * from by_target; drop table by_target; alter table t rename to by_target;
+select count(*) from by_target;
+
+-- insert into by_target select l.nr, filter(l.parts, lambda x,n: not (x in r.parts)) parts, l.eq-r.eq eq, l.btns from by_target l, by_target r where l.nr=r.nr and l.parts @> r.parts and l.parts != r.parts order by l.nr;
+-- create table t as select distinct * from by_target; drop table by_target; alter table t rename to by_target;
+-- select count(*) from by_target;
+--
+-- insert into by_target select l.nr, filter(l.parts, lambda x,n: not (x in r.parts)) parts, l.eq-r.eq eq, l.btns from by_target l, by_target r where l.nr=r.nr and l.parts @> r.parts and l.parts != r.parts order by l.nr;
+-- create table t as select distinct * from by_target; drop table by_target; alter table t rename to by_target;
+-- select count(*) from by_target;
+--
+-- insert into by_target select l.nr, filter(l.parts, lambda x,n: not (x in r.parts)) parts, l.eq-r.eq eq, l.btns from by_target l, by_target r where l.nr=r.nr and l.parts @> r.parts and l.parts != r.parts order by l.nr;
+-- create table t as select distinct * from by_target; drop table by_target; alter table t rename to by_target;
+-- select count(*) from by_target;
+
+-- select [[nr], parts] from by_target as l,generate_series(1,10) gs(i)
+--     where exists (select 1 from by_target r where l.nr=r.nr and parts=[i])
+--       and i in l.parts
+--     and nr=1;
+
+create table uncons as
+    select * from by_target l
+        where not exists (select 1 from generate_series(1,10) gs(i)
+          where exists (select 1 from by_target r where l.nr=r.nr and parts=[i])
+            and i in l.parts);
+
+-- delete from by_target l
+--     where exists (select 1 from generate_series(1,10) gs(i)
+--         where exists (select 1 from by_target r where l.nr=r.nr and parts=[i])
+--           and i in l.parts);
+
+select *, reduce(l.parts, lambda acc, x: acc + (case when x in r.parts then 1 else 0 end), 0) c
+from uncons l, uncons r
+where l.nr=r.nr
+  and l.nr=2
+  and l.eq < r.eq
+  and length(l.parts)=length(r.parts)
+  and reduce(l.parts, lambda acc, x: acc + (case when x in r.parts then 1 else 0 end), 0) = length(l.parts)-1
+order by l.parts;
+-- select nr, count(distinct n) from uncons, unnest(parts) u(n) group by 1 order by 1;
