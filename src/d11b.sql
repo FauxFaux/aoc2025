94a851677bc0babd2a4db06409861ec9eec05f99
Chris West (Faux) <git@goeswhere.com>
2025-12-11 11:39:32 +0000

    d11b

--- src/d11b.sql
+++ src/d11b.sql
@@ -9,19 +9,101 @@ create table raw as
 
 create table edges as
 select arr[1] f, t from raw,unnest(arr[2:]) u(t) order by f, t;
+select count(*) total_edges from edges;
+
+-- assumption: there's only paths from fft -> dac, none from dac -> fft (as that'd cycle)
+
+
+-- create table until_fft as
+--     select * from edges where not (f = 'fft' or t = 'out' or f = 'dac' or t = 'dac');
 --
--- with recursive found(f, t, step, r) as (
---     select *, 1 as step, [row_number() over (order by f, t)] as r from edges where f='svr'
--- union all
---     select e.f, e.t, step + 1, r||[row_number() over (order by e.f, e.t)]
---     from edges e
+-- create table until_dac as
+--     select * from edges where not (f = 'dac' or t = 'out' or t = 'fft');
+--
+-- with recursive found(f, t, r) as (
+--     select * from until_fft where f='svr'
+-- union
+--     select e.f, e.t
+--     from until_fft e
 --     join found fr on fr.t = e.f
--- ) select * from found;
+-- ) select * from found where t='fft';
+--
+-- with recursive found(f, t, r) as (
+--     select * from until_dac where f='fft'
+-- union
+--     select e.f, e.t
+--     from until_dac e
+--     join found fr on fr.t = e.f
+-- ) select * from found where t='dac';
+
+create table reachable_from_fft as
+    with recursive found(f, t, r) as (
+        select * from edges where f='fft'
+    union
+        select e.f, e.t
+        from edges e
+        join found fr on fr.t = e.f
+    ) select * from found;
+
+create table reachable_from_dac as
+    with recursive found(f, t, r) as (
+        select * from edges where f='dac'
+    union
+        select e.f, e.t
+        from edges e
+        join found fr on fr.t = e.f
+    ) select * from found;
+
+create table svr_to_fft as
+    select * from edges
+        except select * from reachable_from_fft;
+
+create table fft_to_dac as
+    select * from reachable_from_fft
+        except select * from reachable_from_dac;
+
+select count(*) fft_able from reachable_from_fft;
+select count(*) dac_able from reachable_from_dac;
+
+---
+
+select count(*) s2f from svr_to_fft;
+select count(*) f2d from fft_to_dac;
+select count(*) d2o from reachable_from_dac;
+
+---
 
 with recursive found(f, t, r) as (
-    select *, [t] as r from edges where f='svr'
+    select * from svr_to_fft where f='svr'
 union all
-    select e.f, e.t,  r||[e.t]
-    from edges e
+    select e.f, e.t
+    from svr_to_fft e
     join found fr on fr.t = e.f
-) select count(*) from found where t='out' and r @> ['fft'] and r @> ['dac'];
+) select count(*) as s2ff from found where t='fft';
+
+with recursive found(f, t, r) as (
+    select * from reachable_from_dac where f='dac'
+union all
+    select e.f, e.t
+    from reachable_from_dac e
+    join found fr on fr.t = e.f
+) select count(*) as d2of from found where t='out';
+
+create table fd as select * from fft_to_dac;
+-- select * from fd o where not exists (select 1 from fd i where i.f = o.t and i.t != 'dac');
+delete from fd o where t != 'dac' and not exists (select 1 from fd i where i.f = o.t);
+delete from fd o where t != 'dac' and not exists (select 1 from fd i where i.f = o.t);
+delete from fd o where t != 'dac' and not exists (select 1 from fd i where i.f = o.t);
+delete from fd o where t != 'dac' and not exists (select 1 from fd i where i.f = o.t);
+delete from fd o where t != 'dac' and not exists (select 1 from fd i where i.f = o.t);
+delete from fd o where t != 'dac' and not exists (select 1 from fd i where i.f = o.t);
+
+with recursive found(f, t, r) as (
+    select * from fd where f='fft'
+    union all
+    select e.f, e.t
+    from fd e
+             join found fr on fr.t = e.f
+) select count(*) f2df from found where t='dac';
+
+-- s2ff*f2df*d2of
